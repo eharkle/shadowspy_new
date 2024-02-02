190751d421fea346da33b71e62c12c35ada05d55
Stefano Bertone <sbertone@umd.edu>
2024-02-02 12:23:17 -0500

    extend read_img_properties to accept columns arg

--- shadowspy/image_util.py
+++ shadowspy/image_util.py
@@ -1,6 +1,6 @@
-def read_img_properties(imgs, cumindex):
+def read_img_properties(imgs, cumindex, columns=['PRODUCT_ID', 'START_TIME']):
 
-    cumindex_red = cumindex[['PRODUCT_ID', 'START_TIME']]
+    cumindex_red = cumindex[columns]
     cumindex_red = cumindex_red.loc[cumindex.PRODUCT_ID.str.strip().isin(imgs)].reset_index(drop=True)
     cumindex_red.PRODUCT_ID = cumindex_red.PRODUCT_ID.str.strip()
 
